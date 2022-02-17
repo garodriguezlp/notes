923912b0c67b08f65b00a9faec115eee5223c9f6
Gustavo Adolfo Rodriguez Libreros <user.email=1432287+garodriguezlp@users.noreply.github.com>
2022-02-16 21:34:11 -0500

    Complete implementation for initial version

--- notes.java
+++ notes.java
@@ -1,16 +1,30 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS info.picocli:picocli:4.5.0
 //DEPS org.apache.commons:commons-lang3:3.11
+//DEPS commons-io:commons-io:2.11.0
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Callable;
 
-import org.apache.commons.lang3.SystemUtils;
+import static java.lang.String.format;
+import static java.lang.System.*;
+import static org.apache.commons.io.FileUtils.writeStringToFile;
+import static org.apache.commons.io.FilenameUtils.removeExtension;
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
+// Font Name: Basic
 @Command(name = "notes", mixinStandardHelpOptions = true, version = "notes 0.1", description = "notes made with jbang", header = {
         "",
         "d8b   db  .d88b.  d888888b d88888b .d8888.",
@@ -23,27 +37,91 @@ import org.apache.commons.lang3.SystemUtils;
 })
 class notes implements Callable<Integer> {
 
-    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
-    private String greeting;
+    private static List<String> HEADER = List.of(
+            "",
+            "d8b   db  .d88b.  d888888b d88888b .d8888.",
+            "888o  88 .8P  Y8. `~~88~~' 88'     88'  YP",
+            "88V8o 88 88    88    88    88ooooo `8bo.",
+            "88 V8o88 88    88    88    88~~~~~   `Y8b.",
+            "88  V888 `8b  d8'    88    88.     db   8D",
+            "VP   V8P  `Y88P'     YP    Y88888P `8888Y'",
+            "");
+
+    private static final String CHARSET = "UTF-8";
+    private static final String FILE_NAME_FORMAt = "yy.MM.dd";
+    private static final String FILE_WITH_SUFFIX_NAME_FORMAT = "yyyy.MM.dd'T'HH.mm";
+    private static final String MONTH_FOLDER_FORMAT = "yyyy.MM";
+
+    @Option(names = {"-w", "--workspace"},
+            required = true,
+            paramLabel = "WORKSPACE",
+            defaultValue = "${env:NOTES_WORKSPACE}",
+            description = "The workspace dir. Defaults to 'env:NOTES_WORKSPACE'")
+    private File workspace;
+
+    @Parameters(index = "0", arity = "0..1", description = "The suffix")
+    private String suffix;
+
+    @Option(names = {"-e", "--editor"},
+            required = true,
+            paramLabel = "EDITOR",
+            defaultValue = "code",
+            description = "The text code editor")
+    private String editor;
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new notes()).execute(args);
-        System.exit(exitCode);
+        exit(exitCode);
     }
 
     @Override
-    public Integer call() throws Exception { // your business logic goes here...
-        System.out.println("Hello " + greeting);
-
-        String[] cmd;
-        final String editorCommand = "code";
-        if (SystemUtils.IS_OS_UNIX) {
-            cmd = new String[] { "sh", "-c", editorCommand };
-        } else {
-            cmd = new String[] { "cmd", "/c", editorCommand };
+    public Integer call() {
+        printHeader();
+        try {
+            File notesFile = buildNoteFile();
+            if (!notesFile.exists()) {
+                writeStringToFile(notesFile, format("# %s", removeExtension(notesFile.getName())), CHARSET);
+            }
+            openEditor(notesFile.getAbsolutePath());
+        } catch (Exception ex) {
+            err.printf("ERROR: %s%n", ex.getMessage());
         }
-        System.out.println("Running `" + String.join(" ", cmd) + "`");
-        new ProcessBuilder(cmd).start();
         return 0;
+    }
+
+    private void printHeader() {
+        out.println(String.join(lineSeparator(), HEADER));
+        out.println("--- ----------------------------------------------------------------------------");
+        out.println("Input parameters:");
+        out.println("workspace: " + workspace);
+        if (isNotEmpty(suffix)) {
+            out.println("suffix: " + suffix);
+        }
+        out.println("--- ----------------------------------------------------------------------------");
+        out.println("");
+    }
+
+    private File buildNoteFile() {
+        LocalDateTime today = LocalDateTime.now();
+        String monthFolder = today.format(DateTimeFormatter.ofPattern(MONTH_FOLDER_FORMAT));
+        String fileName = buildFileName(today);
+        return FileUtils.getFile(workspace, monthFolder, format("%s.md", fileName));
+    }
+
+    private String buildFileName(LocalDateTime today) {
+        return Optional.ofNullable(suffix)
+                .map(s -> format("%s-%s", today.format(DateTimeFormatter.ofPattern(FILE_WITH_SUFFIX_NAME_FORMAT)), s.trim().toUpperCase()))
+                .orElseGet(() -> today.format(DateTimeFormatter.ofPattern(FILE_NAME_FORMAt)));
+    }
+
+    private void openEditor(final String notesFile) throws IOException {
+        String[] cmd;
+        if (SystemUtils.IS_OS_UNIX) {
+            cmd = new String[]{"sh", "-c", editor, notesFile};
+        } else {
+            cmd = new String[]{"cmd", "/c", editor, notesFile};
+        }
+        out.println("Running `" + String.join(" ", cmd) + "`");
+        new ProcessBuilder(cmd).start();
     }
 }
