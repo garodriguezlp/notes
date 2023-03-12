538d545546a4f40a603334fe11d3cbe3b9153d07
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2023-03-12 06:32:39 -0500

    Add support for multiple workspaces

--- notes.java
+++ notes.java
@@ -1,16 +1,23 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
+
 //JAVA 11
+
 //DEPS info.picocli:picocli:4.6.3
 //DEPS org.apache.commons:commons-lang3:3.12.0
 //DEPS commons-io:commons-io:2.11.0
+//DEPS org.slf4j:slf4j-api:2.0.6
+//DEPS org.slf4j:slf4j-simple:2.0.6
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.SystemUtils;
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
-import picocli.CommandLine.PropertiesDefaultProvider;
+//RUNTIME_OPTIONS -Dorg.slf4j.simpleLogger.showLogName=false
+//RUNTIME_OPTIONS -Dorg.slf4j.simpleLogger.showThreadName=false
+
+import static java.lang.String.format;
+import static java.lang.System.lineSeparator;
+import static java.time.Clock.systemDefaultZone;
+import static java.util.stream.Collectors.joining;
+import static org.apache.commons.io.FileUtils.writeStringToFile;
+import static org.apache.commons.io.FilenameUtils.removeExtension;
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,172 +26,231 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.Callable;
-
-import static java.lang.String.format;
-import static java.lang.System.*;
-import static java.time.Clock.systemDefaultZone;
-import static java.util.stream.Collectors.joining;
-import static org.apache.commons.io.FileUtils.writeStringToFile;
-import static org.apache.commons.io.FilenameUtils.removeExtension;
-import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+import picocli.CommandLine.ArgGroup;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.PropertiesDefaultProvider;
 
 // Font Name: Basic
 @Command(name = "notes",
-        mixinStandardHelpOptions = true,
-        version = "notes 0.1",
-        description = "notes made with jbang",
-        defaultValueProvider = PropertiesDefaultProvider.class,
-        header = {
-                "",
-                "d8b   db  .d88b.  d888888b d88888b .d8888.",
-                "888o  88 .8P  Y8. `~~88~~' 88'     88'  YP",
-                "88V8o 88 88    88    88    88ooooo `8bo.",
-                "88 V8o88 88    88    88    88~~~~~   `Y8b.",
-                "88  V888 `8b  d8'    88    88.     db   8D",
-                "VP   V8P  `Y88P'     YP    Y88888P `8888Y'",
-                ""
-        })
-class notes implements Callable<Integer> {
+    mixinStandardHelpOptions = true,
+    version = "notes 0.2",
+    description = "notes made with jbang",
+    defaultValueProvider = PropertiesDefaultProvider.class,
+    showDefaultValues = true,
+    usageHelpAutoWidth = true,
+    header = {
+        "",
+        "d8b   db  .d88b.  d888888b d88888b .d8888.",
+        "888o  88 .8P  Y8. `~~88~~' 88'     88'  YP",
+        "88V8o 88 88    88    88    88ooooo `8bo.",
+        "88 V8o88 88    88    88    88~~~~~   `Y8b.",
+        "88  V888 `8b  d8'    88    88.     db   8D",
+        "VP   V8P  `Y88P'     YP    Y88888P `8888Y'",
+        ""
+    })
+class notes implements Runnable {
 
-    private static List<String> HEADER = List.of(
-            "",
-            "d8b   db  .d88b.  d888888b d88888b .d8888.",
-            "888o  88 .8P  Y8. `~~88~~' 88'     88'  YP",
-            "88V8o 88 88    88    88    88ooooo `8bo.",
-            "88 V8o88 88    88    88    88~~~~~   `Y8b.",
-            "88  V888 `8b  d8'    88    88.     db   8D",
-            "VP   V8P  `Y88P'     YP    Y88888P `8888Y'",
-            "");
+  private static Logger logger = LoggerFactory.getLogger(notes.class);
 
-    private static final String CHARSET = "UTF-8";
-    private static final String FILE_NAME_FORMAT = "yy.MM.dd";
-    private static final String FILE_WITH_SUFFIX_NAME_FORMAT = "yyyy.MM.dd'T'HH.mm";
-    private static final String MONTH_FOLDER_FORMAT = "yyyy.MM";
+  private static List<String> HEADER = List.of("",
+      "",
+      "d8b   db  .d88b.  d888888b d88888b .d8888.",
+      "888o  88 .8P  Y8. `~~88~~' 88'     88'  YP",
+      "88V8o 88 88    88    88    88ooooo `8bo.",
+      "88 V8o88 88    88    88    88~~~~~   `Y8b.",
+      "88  V888 `8b  d8'    88    88.     db   8D",
+      "VP   V8P  `Y88P'     YP    Y88888P `8888Y'",
+      "");
 
-    private final Clock clock;
+  private static final String CHARSET = "UTF-8";
+  private static final String FILE_NAME_FORMAT = "yy.MM.dd";
+  private static final String FILE_WITH_SUFFIX_NAME_FORMAT = "yyyy.MM.dd'T'HH.mm";
+  private static final String CONTAINING_MONTH_FOLDER_FORMAT = "yyyy.MM";
+  private static final String PROPERTIES_DEFAULT_PROVIDER_DES = "property on '${sys:user.home}${sys:file.separator}.notes.properties'";
 
-    @Option(names = {"-w", "--workspace"},
-            required = true,
-            paramLabel = "WORKSPACE",
-            defaultValue = "${notes.workspace}",
-            description = "The workspace dir. Defaults to `workspace` property on '${sys:user.home}${sys:file.separator}.notes.properties'")
-    private File workspace;
+  private final Clock clock;
 
-    @Parameters(index = "*", description = "The issue title")
-    private List<String> suffixes;
+  @Option(names = "--default-workspace",
+      required = true,
+      descriptionKey = "workspace.default",
+      defaultValue = "${notes.workspace.default}",
+      description = "The workspace dir. Defaults to `workspace.default` "
+          + PROPERTIES_DEFAULT_PROVIDER_DES)
+  private File defaultWorkspace;
 
-    @Option(names = {"-e", "--editor"},
-            required = true,
-            paramLabel = "EDITOR",
-            defaultValue = "${notes.editor:-code}",
-            description = "The text code editor. Defaults to `${notes.editor:-code}`")
-    private String notesEditor;
+  @Option(names = "--work-workspace",
+      descriptionKey = "workspace.work",
+      defaultValue = "${notes.workspace.work}",
+      description = "The workspace dir. Defaults to `workspace.work` "
+          + PROPERTIES_DEFAULT_PROVIDER_DES)
+  private File workWorkspace;
 
-    @Option(names = {"-x", "--extension"},
-            required = true,
-            paramLabel = "EXTENSION",
-            defaultValue = "${notes.extension:-md}",
-            description = "The notes file extension. Defaults to `${notes.extension:-md}`")
-    public String extension;
+  @Option(names = "--personal-workspace",
+      descriptionKey = "workspace.personal",
+      defaultValue = "${notes.workspace.personal}",
+      description = "The workspace dir. Defaults to `workspace.personal` "
+          + PROPERTIES_DEFAULT_PROVIDER_DES)
+  private File personalWorkspace;
 
-    @Option(names = {"-s", "--skip-header"},
-            required = true,
-            paramLabel = "SKIP HEADER",
-            defaultValue = "${notes.skipHeader:-false}",
-            description = "Skip the file header generation. Defaults to `${notes.skipHeader:-false}`")
-    public boolean skipHeader;
+  @ArgGroup
+  private ActiveWorkspace activeWorkspace;
 
-    @Option(names = {"-c", "--comment-chars-sequence"},
-            required = true,
-            paramLabel = "COMMENT_CHARS_SEQUENCE",
-            defaultValue = "${notes.commentSequence:-#}",
-            description = "The char sequence to use for comments. Defaults to `${notes.commentSequence:-#}`")
-    public String commentSequence;
+  public static class ActiveWorkspace {
 
-    @Option(names = {"-o", "--open-editor"},
-            required = true,
-            paramLabel = "OPEN_EDITOR",
-            defaultValue = "${notes.openEditor:-true}",
-            description = "Whether or not to open the text editor. Defaults to `${notes.openEditor:-#}`")
-    public boolean openEditor;
+    @Option(names = {"-w", "--work"}, description = "Use the work workspace")
+    private Boolean work;
 
-    public notes(Clock clock) {
-        this.clock = clock;
+    @Option(names = {"-p", "--personal"}, description = "Use the personal workspace")
+    private Boolean personal;
+  }
+
+  @Option(names = "--extension",
+      required = true,
+      descriptionKey = "extension",
+      defaultValue = "${notes.extension:-md}",
+      description = "The notes file extension")
+  public String extension;
+
+  @Option(names = "--skip-header",
+      required = true,
+      descriptionKey = "header.skip",
+      defaultValue = "${notes.header.skip:-false}",
+      description = "Skip the file header generation")
+  public boolean skipHeader;
+
+  @Option(names = "--header-prefix",
+      required = true,
+      descriptionKey = "header.prefix",
+      defaultValue = "${notes.header.prefix:-#}",
+      description = "The char sequence to use for comments")
+  public String headerPrefix;
+
+  @Option(names = "--open-editor",
+      required = true,
+      descriptionKey = "editor.open",
+      defaultValue = "${notes.editor.open:-true}",
+      description = "Whether or not to open the text editor")
+  boolean openEditor;
+
+  @Option(names = "--editor",
+      required = true,
+      descriptionKey = "editor",
+      defaultValue = "${notes.editor:-code}",
+      description = "The text code editor")
+  String editor;
+
+  @Parameters(index = "*", description = "The note description")
+  private List<String> descriptionList;
+
+  public notes(Clock clock) {
+    this.clock = clock;
+  }
+
+  public static void main(String... args) {
+    new CommandLine(new notes(systemDefaultZone())).execute(args);
+  }
+
+  @Override
+  public void run() {
+    printArt();
+    File activeWorkspace = determineActiveWorkspace();
+    String description = buildDescription();
+    printInfo(description, activeWorkspace);
+    try {
+      File notesFile = buildNoteFile(description, activeWorkspace);
+      writeHeader(notesFile);
+      openEditor(notesFile.getAbsolutePath());
+    } catch (Exception ex) {
+      logger.error("Error while creating the note file", ex);
+    }
+  }
+
+  private static void printArt() {
+    logger.info(String.join(lineSeparator(), HEADER));
+  }
+
+  private File determineActiveWorkspace() {
+    if (activeWorkspace == null) {
+      logger.info("No active workspace specified. Using default workspace");
+      return defaultWorkspace;
     }
 
-    public static void main(String... args) {
-        int exitCode = new CommandLine(new notes(systemDefaultZone())).execute(args);
-        exit(exitCode);
+    if (activeWorkspace.work != null && activeWorkspace.work && workWorkspace != null) {
+      logger.info("Active workspace is work. Using work workspace");
+      return workWorkspace;
     }
 
-    @Override
-    public Integer call() {
-        String suffix = buildSuffix();
-        printHeader(suffix);
-        try {
-            File notesFile = buildNoteFile(suffix);
-            writeDefaultHeader(notesFile);
-            openEditor(notesFile.getAbsolutePath());
-        } catch (Exception ex) {
-            err.printf("ERROR: %s%n", ex.getMessage());
-        }
-        return 0;
+    if (activeWorkspace.personal != null && activeWorkspace.personal && personalWorkspace != null) {
+      logger.info("Active workspace is personal. Using personal workspace");
+      return personalWorkspace;
     }
 
-    private String buildSuffix() {
-        return Optional.ofNullable(suffixes)
-                .map(s -> s.stream()
-                        .map(suffix1 -> suffix1.trim().toUpperCase())
-                        .collect(joining("-")))
-                .orElse(null);
+    logger.warn("No valid workspace specified. Using default workspace");
+    return defaultWorkspace;
+  }
+
+  private String buildDescription() {
+    return Optional.ofNullable(descriptionList)
+        .map(s -> s.stream()
+            .map(suffix1 -> suffix1.trim().toUpperCase())
+            .collect(joining("-")))
+        .orElse(null);
+  }
+
+  private void printInfo(String description, File activeWorkspace) {
+    logger.info("--- ----------------------------------------------------------------------------");
+    logger.info("Input parameters:");
+    logger.info("workspace: {}", activeWorkspace);
+    if (isNotEmpty(description)) {
+      logger.info("description: {}", description);
     }
+    logger.info("--- ----------------------------------------------------------------------------");
+    logger.info("");
+  }
 
-    private void printHeader(String suffix) {
-        out.println(String.join(lineSeparator(), HEADER));
-        out.println("--- ----------------------------------------------------------------------------");
-        out.println("Input parameters:");
-        out.println("workspace: " + workspace);
-        if (isNotEmpty(suffix)) {
-            out.println("suffix: " + suffix);
-        }
-        out.println("--- ----------------------------------------------------------------------------");
-        out.println("");
+  private File buildNoteFile(String suffix, File activeWorkspace) {
+    LocalDateTime today = LocalDateTime.now(clock);
+    String containingFolder = today.format(
+        DateTimeFormatter.ofPattern(CONTAINING_MONTH_FOLDER_FORMAT));
+    String fileName = buildFileName(today, suffix);
+    return FileUtils.getFile(activeWorkspace, containingFolder,
+        format("%s.%s", fileName, extension));
+  }
+
+  private String buildFileName(LocalDateTime today, Object suffix) {
+    return Optional.ofNullable(suffix)
+        .map(s -> format("%s-%s",
+            today.format(DateTimeFormatter.ofPattern(FILE_WITH_SUFFIX_NAME_FORMAT)), suffix))
+        .orElseGet(() -> today.format(DateTimeFormatter.ofPattern(FILE_NAME_FORMAT)));
+  }
+
+  private void writeHeader(File notesFile) throws IOException {
+    if (!skipHeader) {
+      String header = removeExtension(notesFile.getName());
+      writeStringToFile(notesFile, format("%s %s", headerPrefix, header), CHARSET);
     }
+  }
 
-    private File buildNoteFile(String suffix) {
-        LocalDateTime today = LocalDateTime.now(clock);
-        String monthFolder = today.format(DateTimeFormatter.ofPattern(MONTH_FOLDER_FORMAT));
-        String fileName = buildFileName(today, suffix);
-        return FileUtils.getFile(workspace, monthFolder, format("%s.%s", fileName, extension));
+  private void openEditor(final String notesFile) throws IOException {
+    String filePath = notesFile.contains(" ") ? format("\"%s\"", notesFile) : notesFile;
+    String editorCmd = String.join(" ", editor, filePath);
+
+    List<String> cmd = SystemUtils.IS_OS_UNIX ?
+        List.of("sh", "-c", editorCmd) :
+        List.of("cmd", "/c", editorCmd);
+
+    if (openEditor) {
+      logger.debug("Running `{}`", cmd);
+      new ProcessBuilder(cmd).start();
+    } else {
+      logger.info("The editor was not open because `--open-editor` is set as `false`");
     }
-
-    private String buildFileName(LocalDateTime today, Object suffix) {
-        return Optional.ofNullable(suffix)
-                .map(s -> format("%s-%s", today.format(DateTimeFormatter.ofPattern(FILE_WITH_SUFFIX_NAME_FORMAT)), suffix))
-                .orElseGet(() -> today.format(DateTimeFormatter.ofPattern(FILE_NAME_FORMAT)));
-    }
-
-    private void writeDefaultHeader(File notesFile) throws IOException {
-        if (!notesFile.exists() && !skipHeader) {
-            writeStringToFile(notesFile, format("%s %s", commentSequence, removeExtension(notesFile.getName())), CHARSET);
-        }
-    }
-
-    private void openEditor(final String notesFile) throws IOException {
-        String filePath = notesFile.contains(" ") ? format("\"%s\"", notesFile) : notesFile;
-        String editorCmd = String.join(" ", notesEditor, filePath);
-
-        List<String> cmd = SystemUtils.IS_OS_UNIX ?
-                List.of("sh", "-c", editorCmd) :
-                List.of("cmd", "/c", editorCmd);
-
-        out.println("Running `" + String.join(" ", cmd) + "`");
-
-        if (openEditor) {
-            new ProcessBuilder(cmd).start();
-        } else {
-            out.println("--open-editor is set as `false`, therefore no editor was open");
-        }
-    }
+  }
 }
